8e29eee495722de94dbb754f0d6b40c2d011040e
Bruno Brancalhao <brunobrancalhao26@gmail.com>
2020-07-21 23:23:06 -0300

    initialize providers

--- lib/screens/workout_screen.dart
+++ lib/screens/workout_screen.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import './workout_management_screen.dart';
 import '../widgets/workout_screen_custom_clipper.dart';
 import '../widgets/app_drawer.dart';
 import '../screens/exercise_screen.dart';
+
+import '../providers/workout_providers.dart';
 
 class WorkoutScreen extends StatelessWidget {
   static const route = '/workout';
 
   @override
   Widget build(BuildContext context) {
+    print('build workout');
     final mediaQuery = MediaQuery.of(context);
 
     return Scaffold(
@@ -37,56 +42,63 @@ class WorkoutScreen extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.only(top: 80),
-            child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Row(
-                children: <Widget>[
-                  SizedBox(
-                    width: mediaQuery.size.width * 0.4,
-                    child: ClipPath(
-                      clipper: WorkoutScreenCustomClipper(),
-                      child: Image(
-                        image: NetworkImage(
-                            'https://www.folhavitoria.com.br/esportes/blogs/corridaderua/wp-content/uploads/2019/06/WhatsApp-Image-2019-06-06-at-19.22.37.jpeg'),
-                        height: 150,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
+            child: Consumer<WorkoutProvider>(
+              builder: (_, provider, child) {
+                print('consumer');
+                print(child);
+                return Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Text(
-                          'Corrida',
-                          style: Theme.of(context).textTheme.headline4,
-                        ),
-                        Text(
-                          'Sábado',
-                          style: Theme.of(context).textTheme.subtitle2,
-                        ),
-                        SizedBox(
-                          width: double.infinity,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: OutlineButton(
-                              onPressed: () => Navigator.of(context)
-                                  .pushNamed(ExerciseScreen.route),
-                              borderSide: BorderSide(
-                                color: Color.fromRGBO(0, 223, 100, 1),
-                              ),
-                              child: Text('Exercicios'),
-                            ),
+                  child: Row(
+                    children: <Widget>[
+                      SizedBox(
+                        width: mediaQuery.size.width * 0.4,
+                        child: ClipPath(
+                          clipper: WorkoutScreenCustomClipper(),
+                          child: Image(
+                            image: NetworkImage(
+                                'https://www.folhavitoria.com.br/esportes/blogs/corridaderua/wp-content/uploads/2019/06/WhatsApp-Image-2019-06-06-at-19.22.37.jpeg'),
+                            height: 150,
+                            fit: BoxFit.cover,
                           ),
                         ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Text(
+                              'Corrida',
+                              style: Theme.of(context).textTheme.headline4,
+                            ),
+                            Text(
+                              'Sábado',
+                              style: Theme.of(context).textTheme.subtitle2,
+                            ),
+                            SizedBox(
+                              width: double.infinity,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: OutlineButton(
+                                  onPressed: () => Navigator.of(context)
+                                      .pushNamed(ExerciseScreen.route),
+                                  borderSide: BorderSide(
+                                    color: Color.fromRGBO(0, 223, 100, 1),
+                                  ),
+                                  child: Text('Exercicios'),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              },
+              child: Text('child'),
             ),
           ),
         ],
